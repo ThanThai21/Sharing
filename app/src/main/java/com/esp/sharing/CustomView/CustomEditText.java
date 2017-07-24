66ed73642fe240d26367e7902df2b3bffef6e6a3
ThanThai21 <thaibk210@gmail.com>
2017-07-24 20:55:53 +0700

    customize font for edit text

---
+++ app/src/main/java/com/esp/sharing/CustomView/CustomEditText.java
@@ -0,0 +1,40 @@
+package com.esp.sharing.CustomView;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.EditText;
+
+public class CustomEditText extends EditText{
+    public CustomEditText(Context context) {
+        super(context);
+        init();
+    }
+
+    public CustomEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public CustomEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public CustomEditText(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    private void init() {
+        if (!isInEditMode()) {
+            Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "font.ttf");
+            setTypeface(tf);
+        }
+    }
+}
